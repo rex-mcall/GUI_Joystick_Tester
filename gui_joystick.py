9c7fe20684f9fab59d181e5aa46138a2e918dffd
rex-mcall <rexmcall@gmail.com>
2021-05-01 17:21:43 -0400

    Added a vector line

--- gui_joystick.py
+++ gui_joystick.py
@@ -33,7 +33,7 @@ def initializeDisplay():
 
 def drawXInput(window, controller):
     pygame.event.pump()
-    width = 10
+    width = 4
     height = (0 - (100 * controller.get_axis(1)))
     xPosition = (pygame.display.get_window_size()[0]/2) - (width/2)
     yPosition = 0
@@ -45,8 +45,8 @@ def drawXInput(window, controller):
 
 def drawYInput(window, controller):
     pygame.event.pump()
-    width = (0 - (100 * controller.get_axis(2)))
-    height = 10
+    width = (0 - (100 * controller.get_axis(0)))
+    height = 4
     xPosition = 0
     yPosition = (pygame.display.get_window_size()[1]/2) - (height/2)
     if width >= 0:
@@ -55,6 +55,11 @@ def drawYInput(window, controller):
         xPosition = 450
     pygame.draw.rect(window,blue,(xPosition, yPosition, abs(width), height))
 
+def drawAxisBox(window):
+    # Draw a rectangle outline
+    pygame.draw.rect(window, black, [350, 100, 200, 200], 2)
+    pygame.draw.line(window, black, (350, 199), (550,199), 2)
+    pygame.draw.line(window, black, (449, 100), (449, 300), 2)
 def main():
     pygame.init()
 
@@ -63,8 +68,11 @@ def main():
     
     while True:
         window.fill(white)
-        pygame.draw.rect(window,blue,(445, 195, 10, 10))
-        pygame.draw.rect(window,red,(445 + (100 * controller.get_axis(2)), 195 + (100 * controller.get_axis(1)), 10, 10))
+        pygame.draw.rect(window,blue,(448, 198, 4, 4))
+        pygame.draw.line(window, green, (449,199), (449+(100 * controller.get_axis(0)), 199+(100 * controller.get_axis(1))), 2)
+        pygame.draw.rect(window,red,(448 + (100 * controller.get_axis(0)), 198 + (100 * controller.get_axis(1)), 4, 4))
+        pygame.draw.line(window, green, (449,199), (449+(100 * controller.get_axis(0)), 199+(100 * controller.get_axis(1))), 2)
+        drawAxisBox(window)
         drawXInput(window, controller)
         drawYInput(window, controller)
         for event in pygame.event.get():
