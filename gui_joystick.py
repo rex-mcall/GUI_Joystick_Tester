bcf7c99f0d75f99f31701fe7718280cccec52ebd
rex-mcall <rexmcall@gmail.com>
2021-05-01 21:39:11 -0400

    Changed to dark mode and added scaling variables

--- gui_joystick.py
+++ gui_joystick.py
@@ -5,12 +5,23 @@ import time
 from pygame import *
 from pygame.locals import *
 
+# color tuples
 white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
 
+# controller mapping
+fwdBack = 1
+leftRight = 0
+rotate = 4
+
+#display reference
+centerX = 1920 / 2
+centerY = 1080 / 2
+lineWidth = 2
+scale = 400
 def initializeController():
     pygame.joystick.init()
     names = [pygame.joystick.Joystick(x).get_name() for x in range(pygame.joystick.get_count())]
@@ -22,9 +33,17 @@ def initializeController():
     controller.init()
     return controller
 
+def setControllerMap():
+    deviceType = int(input("Choose device type:\n0: XBox\n1: Flight Sim Stick\n> "))
+    if deviceType == 0:
+        pass
+    elif deviceType == 1:
+        pass
 def initializeDisplay():
-    displayWidth = 900
-    displayHeight = 400
+    displayWidth = 1920
+    centerX = displayWidth / 2
+    displayHeight = 1080
+    centerY = displayHeight / 2
     icon = pygame.image.load("jrti.png")
     display = pygame.display.set_mode((displayWidth,displayHeight),0,10)
     pygame.display.set_caption('GUI Joystick Tester')
@@ -33,33 +52,34 @@ def initializeDisplay():
 
 def drawXInput(window, controller):
     pygame.event.pump()
-    width = 4
-    height = (0 - (100 * controller.get_axis(1)))
-    xPosition = (pygame.display.get_window_size()[0]/2) - (width/2)
+    width = lineWidth * 2
+    height = (0 - (scale * controller.get_axis(fwdBack)))
+    xPosition = centerX - (width/2)
     yPosition = 0
     if height >= 0:
-        yPosition = 200 - height
+        yPosition = centerY - height
     else:
-        yPosition = 200
-    pygame.draw.rect(window,blue,(xPosition, yPosition, width, abs(height)))
+        yPosition = centerY
+    pygame.draw.rect(window,red,(xPosition, yPosition, width, abs(height)))
 
 def drawYInput(window, controller):
     pygame.event.pump()
-    width = (0 - (100 * controller.get_axis(0)))
-    height = 4
+    width = (0 - (scale * controller.get_axis(leftRight)))
+    height = lineWidth * 2
     xPosition = 0
-    yPosition = (pygame.display.get_window_size()[1]/2) - (height/2)
+    yPosition = centerY - (height/2)
     if width >= 0:
-        xPosition = 450 - width
+        xPosition = centerX - width
     else:
-        xPosition = 450
-    pygame.draw.rect(window,blue,(xPosition, yPosition, abs(width), height))
+        xPosition = centerX
+    pygame.draw.rect(window,red,(xPosition, yPosition, abs(width), height))
 
 def drawAxisBox(window):
     # Draw a rectangle outline
-    pygame.draw.rect(window, black, [350, 100, 200, 200], 2)
-    pygame.draw.line(window, black, (350, 199), (550,199), 2)
-    pygame.draw.line(window, black, (449, 100), (449, 300), 2)
+    pygame.draw.rect(window, white, [centerX - scale, centerY - scale, 2 * scale, 2 * scale], lineWidth)
+    pygame.draw.line(window, white, (centerX - scale, centerY), (centerX + scale,centerY), lineWidth)
+    pygame.draw.line(window, white, (centerX, centerY - scale), (centerX, centerY + scale), lineWidth)
+
 def main():
     pygame.init()
 
@@ -67,14 +87,13 @@ def main():
     window = initializeDisplay()
     
     while True:
-        window.fill(white)
-        pygame.draw.rect(window,blue,(448, 198, 4, 4))
-        pygame.draw.line(window, green, (449,199), (449+(100 * controller.get_axis(0)), 199+(100 * controller.get_axis(1))), 2)
-        pygame.draw.rect(window,red,(448 + (100 * controller.get_axis(0)), 198 + (100 * controller.get_axis(1)), 4, 4))
-        pygame.draw.line(window, green, (449,199), (449+(100 * controller.get_axis(0)), 199+(100 * controller.get_axis(1))), 2)
+        window.fill(black)
+        pygame.draw.rect(window,blue,(centerX, centerY, lineWidth, lineWidth))
         drawAxisBox(window)
         drawXInput(window, controller)
         drawYInput(window, controller)
+        pygame.draw.line(window, green, (centerX,centerY), (centerX+(scale * controller.get_axis(leftRight)), centerY+(scale * controller.get_axis(fwdBack))), lineWidth)
+        pygame.draw.rect(window,red,(centerX + (scale * controller.get_axis(leftRight)), centerY + (scale * controller.get_axis(fwdBack)), 5, 5)) #final position dot
         for event in pygame.event.get():
             if event.type==QUIT:
                 pygame.quit()
